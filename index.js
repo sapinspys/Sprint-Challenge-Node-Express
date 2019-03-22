fbfb5844c467944ecaa437451261c052efd8c4e1
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-22 11:24:52 -0400

    Fix: imported proper index

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
-const express = require("express");
+require('dotenv').config();
 
-const usersRouter = require("./routers/users-router.js");
-const postsRouter = require("./routers/posts-router.js");
+const server = require('./server.js');
 
-const helmet = require("helmet");
-const cors = require("cors");
+const port = process.env.PORT || 5000;
+const greeting = process.env.GREETING;
 
-const server = express();
-
-// BUILT-IN MIDDLEWARE (WITH HELMET SECURITY)
-server.use(express.json());
-server.use(helmet());
-
-// CUSTOM MIDDLEWARE (LOGGER)
-server.use(function(req, res, next) {
-  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
-
-  next();
-});
-
-// THIRD PARTY MIDDLEWARE
-server.use(cors());
-
-server.get("/", (req, res) => {
-  res.send("Hello from the WEB API III Challenge server!");
-});
-
-// ROUTE HANDLERS ARE ALSO MIDDLEWARE
-server.use("/api/users", usersRouter);
-server.use("/api/posts", postsRouter);
-
-// CUSTOM MIDDLEWARE!
-server.use(function(req, res) {
-  res
-    .status(404)
-    .send("This route does not exist. Ain't nobody got time for that!");
-});
-
-module.exports = server;
+server.listen(port, () => {
+  console.log(`\n*** ${greeting} Server Running on http://localhost:${port} ***\n`)
+})
